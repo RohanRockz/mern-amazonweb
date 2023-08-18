d54fbb05f09ea2627e293ac41db3b4ae7440857a
Rohan Rockz <rohanagarwal2019@gmail.com>
2023-08-18 20:05:13 +0530

    changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser(""));
 app.use(cors());
 app.use(router);
 
-const port=8005;
+const port=process.env.PORT || 8005
 app.listen(port,()=>{
     console.log(`server is running on port number ${port}`);
 });
